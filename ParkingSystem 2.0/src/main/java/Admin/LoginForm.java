22c42506b094c4b5113c209a63d2496c01da5fc2
Dqkhanhbk <kdao179742@troy.edu>
2022-08-09 20:24:13 +0700

    Khanh update icon cho UI

--- ParkingSystem 2.0/src/main/java/Admin/LoginForm.java
+++ ParkingSystem 2.0/src/main/java/Admin/LoginForm.java
@@ -92,6 +92,7 @@ public class LoginForm extends javax.swing.JFrame {
             }
         });
 
+        btnLogIn.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Admin/Login.png"))); // NOI18N
         btnLogIn.setText("Log In");
         btnLogIn.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -101,6 +102,7 @@ public class LoginForm extends javax.swing.JFrame {
 
         jLabel4.setText("Group 5");
 
+        btnQuit.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Admin/Quit.png"))); // NOI18N
         btnQuit.setText("Quit");
         btnQuit.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
